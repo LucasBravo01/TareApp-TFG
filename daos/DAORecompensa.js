3fc1c02afc55141ec3419621250eef2673bcaab9
Cofi <121046362+cofi06@users.noreply.github.com>
2024-04-11 01:58:39 +0200

    getRecompensasUsuario

--- daos/DAORecompensa.js
+++ daos/DAORecompensa.js
@@ -4,6 +4,9 @@ class DAORecompensa {
     // Constructor con las conexiones
     constructor (pool) {
         this.pool = pool;
+
+        this.readAllRecompensasCrearTarea = this.readAllRecompensasCrearTarea.bind(this);
+        this.getRecompensasUsuario = this.getRecompensasUsuario.bind(this);
     }
 
     readAllRecompensas (callback) {
@@ -58,6 +61,41 @@ class DAORecompensa {
                         rows.forEach(element => {
                             id = element.id;
                             titulo = element.titulo;
+
+                            recompensas.push({id, titulo});
+                        });
+
+                        callback(null, recompensas);
+                    }
+                });
+            }
+        });
+    }
+
+    // Obtener las recompensas de un usuario tras haber completado una tarea
+    getRecompensasUsuario (callback) {
+        this.pool.getConnection(function(err, connection) {
+            if (err) {
+                callback(-1);
+            }
+            else {
+                let querySQL = "SELECT REC.* FROM recompensa AS REC JOIN tarea AS TAR ON TAR.id_recompensa = REC.id JOIN actividad AS ACT ON ACT.id = TAR.id_actividad WHERE TAR.terminada = 1 AND ACT.id_creador = ?;";
+                
+                connection.query(querySQL, [idUser], function(err, rows) {
+                    connection.release();
+
+                    if(err) {
+                        callback(-1);
+                    }
+                    else {
+                        let recompensas = [];
+                        let id; let titulo; let icono; let mensaje;
+
+                        rows.forEach(element => {
+                            id = element.id;
+                            titulo = element.titulo;
+                            icono = element.icono;
+                            mensaje = element.mensaje;
 
                             recompensas.push({id, titulo, icono, mensaje});
                         });
