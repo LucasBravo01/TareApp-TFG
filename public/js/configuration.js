8a452b09c95caee5b42c1d39549dce64b989b925
CCalvoM <cacalv04@ucm.es>
2024-04-24 16:06:04 +0200

    feat: modal configuracion

--- public/js/configuration.js
+++ public/js/configuration.js
@@ -3,12 +3,12 @@
 // Validación Cliente
 function validateParams(params) {
     let error = {};
-    const user = $("body").data("user");
+    let user = $("body").data("user");
     // Campos no vacíos
-    if (params.font_size === "" || params.theme === "" || params.time_preference === "") {
+    if (params.font_size === user.configuration.font_size && params.theme === user.configuration.theme && params.time_preference === user.configuration.time_preference) {
         error.code = 400;
-        error.title = "Campos vacíos";
-        error.message = "Asegúrate de rellenar todos los campos.";
+        error.title = "Campos no modificados";
+        error.message = "Los campos de la configuración no han sido modificados.";
         return error;
     }
     else {
@@ -48,6 +48,17 @@ $(() => {
                     $("#img-nav-settings").attr('src', `/images/${themeSelect.val()}/settings.png`);
                     $("#img-nav-logout").attr('src', `/images/${themeSelect.val()}/logout.png`);
                     $("#img-nav-profile").attr('src', `/images/${themeSelect.val()}/default-user.png`);
+
+                    let params = {
+                        id_user: $("body").data("user").id,
+                        font_size: fontSizeSelect.val(),
+                        theme: themeSelect.val(),
+                        time_preference: timeSelect.val()
+                    };
+                    
+                    $("body").data("user").configuration.font_size = params.font_size;
+                    $("body").data("user").configuration.theme = params.theme;
+                    $("body").data("user").configuration.time_preference = params.time_preference;
 
                     // Aplicar configuración
                     setConfiguration(fontSizeSelect.val());
