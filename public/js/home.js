04adce020a690f48c408d548d0cfc4095053e5bc
Cofi <121046362+cofi06@users.noreply.github.com>
2024-04-25 20:19:22 +0200

    voy a modificar todo asiq commit por si las moscas

--- public/js/home.js
+++ public/js/home.js
@@ -36,6 +36,11 @@ $(() => {
 
         $("#a-previous-day").attr("href", `/diaria/${previousDay}`);
         $("#a-next-day").attr("href", `/diaria/${nextDay}`);
+
+        // CAMBIAR CLASES BOTONES
+        $("#a-home-list").attr("class", "me-1 view-non-selected-button");
+        $("#a-home-week").attr("class", "me-1 view-non-selected-button");
+        $("#a-home-daily").attr("class", "me-1 view-selected-button");
     }
 
     const week = $("body").data("week");
@@ -53,5 +58,17 @@ $(() => {
         
         $("#a-previous-week").attr("href", `/semanal/${previousWeek}`);
         $("#a-next-week").attr("href", `/semanal/${nextWeek}`);
+
+        // CAMBIAR CLASES BOTONES
+        $("#a-home-list").attr("class", "me-1 view-non-selected-button");
+        $("#a-home-week").attr("class", "me-1 view-selected-button");
+        $("#a-home-daily").attr("class", "me-1 view-non-selected-button");
+    }
+
+    if(!day && !week) {
+        // CAMBIAR CLASES BOTONES
+        $("#a-home-list").attr("class", "me-1 view-selected-button");
+        $("#a-home-week").attr("class", "me-1 view-non-selected-button");
+        $("#a-home-daily").attr("class", "me-1 view-non-selected-button");
     }
 });
