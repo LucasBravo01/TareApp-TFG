9250fb5c4542cc74d449339bc1b8e2488d279f65
Cofi <121046362+cofi06@users.noreply.github.com>
2024-05-10 16:06:03 +0200

    merge con dev

--- public/js/studysession.js
+++ public/js/studysession.js
@@ -60,28 +60,34 @@ $(() => {
     const studySessions = $("body").data("studySessions");
 
     // Elementos HTML
-    const inputName = $('#input-name');
-    const inputStudySlot = $('#input-study-slot');
-    const inputBrakeSlot = $('#input-brake-slot');
-    const inputNumberSlots = $('#input-number-slots');
-    const inputLongBrakeSlot = $('#input-long-brake-slot');
-    const inputNumberLongBrakeSlot = $('#input-number-long-brake-slots');
+    const inputStudySession = $("#input-study-session");
+    const inputName = $("#input-name");
+    const divInputName = $("#div-input-name");
+    const inputStudySlot = $("#input-study-slot");
+    const inputBrakeSlot = $("#input-brake-slot");
+    const inputNumberSlots = $("#input-number-slots");
+    const inputLongBrakeSlot = $("#input-long-brake-slot");
+    const inputNumberLongBrakeSlot = $("#input-number-long-brake-slots");
+    const buttonCreateStudySession = $("input-sb-createStudySession");
 
-    inputName.on("change", () => {
-        let selectedSession = inputName.val();
+    inputStudySession.on("change", () => {
+        let selectedSession = inputStudySession.val();
 
         if (selectedSession === "Nueva") {
+            inputName.val("");
             inputStudySlot.val("");
             inputBrakeSlot.val("");
             inputNumberSlots.val("");
             inputLongBrakeSlot.val("");
             inputNumberLongBrakeSlot.val("");
 
+            divInputName.attr("style", "display: block;")
             inputStudySlot.removeAttr("disabled");
             inputBrakeSlot.removeAttr("disabled");
             inputNumberSlots.removeAttr("disabled");
             inputLongBrakeSlot.removeAttr("disabled");
             inputNumberLongBrakeSlot.removeAttr("disabled");
+            buttonCreateStudySession.removeAttr("disabled");
         } else {
             let session = studySessions[selectedSession];
 
@@ -91,11 +97,13 @@ $(() => {
             inputLongBrakeSlot.val(session.long_brake_slot);
             inputNumberLongBrakeSlot.val(session.num_long_slots);
 
+            divInputName.attr("style", "display: none;")
             inputStudySlot.attr("disabled", true);
             inputBrakeSlot.attr("disabled", true);
             inputNumberSlots.attr("disabled", true);
             inputLongBrakeSlot.attr("disabled", true);
             inputNumberLongBrakeSlot.attr("disabled", true);
+            buttonCreateStudySession.attr("disabled", true);
         }
     });
 });
