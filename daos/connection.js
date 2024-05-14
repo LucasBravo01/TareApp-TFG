35e6ae43ef3d559e02f97dd00c1d6443040bab73
LucasBravo01 <lucasbravofairen@hotmail.com>
2024-05-14 21:31:25 +0200

    .env

--- daos/connection.js
+++ daos/connection.js
@@ -1,13 +1,15 @@
 "use strict"
 
+require('dotenv').config()
+
 module.exports = {
     mysqlConfig: { // Conexión BBDD
-        host: "localhost",
-        user: "root",
-        password: "",
-        database: "TareApp",
-        port: 3306
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        port: process.env.DB_PORT,
     },
 
-    port: 3000  // Puerto del servidor
+    port: process.env.APP_PORT  // Puerto del servidor
 }
